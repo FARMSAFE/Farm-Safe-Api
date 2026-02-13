f542ef2f9b612ccef964661e0f485dd98f5bc97a
AndrewNyirenda <andrewkaynyirenda@gmail.com>
2026-02-13 22:38:21 +0200

    Integrated With Neon Postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,42 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+        const databaseUrl = configService.get('DATABASE_URL');
+        
+        
+        
+        return {
+          type: 'postgres',
+          url: databaseUrl,
+          ssl: {
+            rejectUnauthorized: false, 
+          },
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: !isProduction,
+          logging: !isProduction,
+          autoLoadEntities: true,
+          extra: {
+            max: 20, 
+            connectionTimeoutMillis: 5000,
+          },
+        };
+      },
+    }),
+  ],
 })
 export class AppModule {}
